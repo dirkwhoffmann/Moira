e8f4f0ee8aa2c70903e90174c4f5e17d32ac10af
Dirk Hoffmann <dirk.hoffmann@me.com>
2022-04-22 09:40:29 +0200

    Fixing CMake build issues on Linux

--- Moira/MoiraExec_cpp.h
+++ Moira/MoiraExec_cpp.h
@@ -1372,6 +1372,7 @@ Moira::execMovepDxEa(u16 opcode)
         {
             writeM <M,Byte> (ea, (dx >> 24) & 0xFF); ea += 2;
             writeM <M,Byte> (ea, (dx >> 16) & 0xFF); ea += 2;
+			[[fallthrough]];
         }
         case Word:
         {
@@ -1399,7 +1400,7 @@ Moira::execMovepEaDx(u16 opcode)
         {
             dx |= readMS <MEM_DATA, Byte> (ea) << 24; ea += 2;
             dx |= readMS <MEM_DATA, Byte> (ea) << 16; ea += 2;
-            // fallthrough
+			[[fallthrough]];
         }
         case Word:
         {
